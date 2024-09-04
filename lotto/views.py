bb475164ab04ff08e57128288f9a713a924069d4
SaltySalt77 <hyna0707@gmail.com>
2024-09-04 15:59:50 +0900

    Feat: Add template for lotto

--- lotto/views.py
+++ lotto/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import GuessNumbers
 
 def index(request) :
-    return HttpResponse('<h1>Hello, world!</h1>')
+    lottos = GuessNumbers.objects.all()
+    return render(request, 'lotto/default.html', {'lottos':lottos})
 
 def hello(request) :
     return HttpResponse('<h1 style="color:red;">Hello world!</h1>')
